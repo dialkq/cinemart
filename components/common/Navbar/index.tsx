3e9013859e6d9dc19b74b9da28e4004d687a128e
dialkq <dialkq@gmail.com>
2024-04-12 22:25:38 +0700

    styling button/theme

--- components/common/Navbar/index.tsx
+++ components/common/Navbar/index.tsx
@@ -1,17 +1,9 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { PiFilmReel } from "react-icons/pi";
-
+import ThemeButton from "@/components/common/Button/ThemeButton";
 
 export function Navbar() {
   const { setTheme } = useTheme();
@@ -26,22 +18,7 @@ export function Navbar() {
       </div>
       {/* TOOGLE THEME */}
       <div className="">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon">
-              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem onClick={() => setTheme("light")}>
-              Light
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("dark")}>
-              Dark
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <ThemeButton />
       </div>
     </div>
   );
