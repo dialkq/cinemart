d5ad35abb534c9cf845e904926e62beb6f6236b4
dialkq <dialkq@gmail.com>
2024-04-28 22:42:04 +0700

    fetch for images in carousel card and styled it

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,42 @@
+"use client";
 import GenreTabs from "@/components/common/GenreTabs";
 import { Navbar } from "@/components/common/Navbar";
 import CarouselCard from "@/components/common/Carousel";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
+
+const queryClient = new QueryClient();
 export default function Home() {
   const genres = ["Action", "Adventure", "Crime", "Romance"].sort();
 
   return (
-    <div className="w-full flex flex-col min-h-screen mx-auto">
-      <div className="w-full mx-auto">
-        <Navbar />
-      </div>
-      {/* MAPING GENRE TABS */}
-      <div
-        className="w-11/12 mx-auto my-3 md:my-4 lg:my-5
+    <QueryClientProvider client={new QueryClient()}>
+      <div className="w-full flex flex-col min-h-screen mx-auto">
+        <div className="w-full mx-auto">
+          <Navbar />
+        </div>
+        {/* MAPING GENRE TABS */}
+        <div
+          className="w-11/12 mx-auto my-3 md:my-4 lg:my-5
       grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4
       gap-x-5 gap-y-3 md:gap-x-8 md:gap-y-3 lg:gap-x-24 lg:gap-y-4"
-      >
-        {genres.map((genre) => (
-          <GenreTabs key={genre} genre={genre} />
-        ))}
+        >
+          {genres.map((genre) => (
+            <GenreTabs key={genre} genre={genre} />
+          ))}
+        </div>
+        <div className="w-11/12 my-3 md:my-5 lg:my-10 md:w-9/12 lg:w-10/12 mx-auto">
+          <p className="text-center font-bold font-mono text-3xl md:text-4xl">
+            Now Playing
+          </p>
+          <CarouselCard />
+        </div>
       </div>
-      <div className="w-11/12 my-3 md:my-5 lg:my-10 md:w-9/12 lg:w-10/12 mx-auto">
-        <p className="text-center font-bold font-mono text-3xl md:text-4xl">~Now Playing~</p>
-        <CarouselCard />
-      </div>
-    </div>
+    </QueryClientProvider>
   );
 }
--- components/common/Carousel/index.tsx
+++ components/common/Carousel/index.tsx
@@ -1,5 +1,4 @@
-import * as React from "react";
-
+"use client";
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
@@ -8,8 +7,35 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import axios from "axios";
+import { useQuery } from "react-query";
+import Image from "next/image";
 
+interface Movie {
+  poster_path: string;
+  title: string;
+}
 export default function CarouselCard() {
+  const fetchMovies = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ODg3MjZiOWQzNzdjMjI1ZmNmNzhhZjEwMTI0YTA4OCIsInN1YiI6IjY1ZGFjMDY0YWUyODExMDE3YzRjMzkwZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NPd90WmkTBur276VZ6-xXX1BWtxMmx77d9DIIBw1cm0",
+      },
+    };
+
+    const response = await axios.get(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      options
+    );
+    return response.data;
+  };
+
+  const { data, isLoading, error } = useQuery("movies", fetchMovies);
+  if (isLoading) return "Loading Movie...";
+
   return (
     <Carousel
       opts={{
@@ -19,12 +45,18 @@ export default function CarouselCard() {
       className="w-full mx-auto"
     >
       <CarouselContent className="">
-        {Array.from({ length: 100 }).map((_, index) => (
+        {data.results.slice(0, 10).map((movie: Movie, index: number) => (
           <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-5 md:p-10 lg:p-10">
+            <div className="p-5 my-3bg-red-100">
               <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-3xl font-semibold">{index + 1}</span>
+                <CardContent className="cursor-pointer flex p-0">
+                  <Image
+                    className="rounded-md"
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.title}
+                    width={500}
+                    height={500}
+                  />
                 </CardContent>
               </Card>
             </div>
@@ -32,7 +64,7 @@ export default function CarouselCard() {
         ))}
       </CarouselContent>
       <CarouselPrevious className="hidden md:flex" />
-      <CarouselNext className="hidden md:flex"/>
+      <CarouselNext className="hidden md:flex" />
     </Carousel>
   );
 }
